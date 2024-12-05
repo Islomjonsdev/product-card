a397eafcb0ee4c911db7bedf9fa2a6faaa912e37
Islomjonsdev <islomjon@immigrateable.com>
2024-12-05 17:51:29 +0500

    post donee

--- src/pages/CreateProduct/CreateProduct.jsx
+++ src/pages/CreateProduct/CreateProduct.jsx
@@ -2,57 +2,83 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { instance } from "../../api";
+import { MdOutlineFileUpload } from "react-icons/md";
 import "./CreateProduct.scss";
 
 const CreateProduct = () => {
   const [postProduct, setPostProduct] = useState([]);
-  const [ title, setTitle ] = useState("")
-  const [ description, setDescription ] = useState("")
-  const [ price, setPrice ] = useState("")
-//   const [ title, setTitle ] = useState("")
-const navigate = useNavigate()
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [photo, setPhoto] = useState(null);
+  const navigate = useNavigate();
 
   const handlePostProduct = (e) => {
     e.preventDefault();
 
     const productData = {
-        title: title,
-        description: description,
-        price: price
+      title: title,
+      description: description,
+      price: price,
     };
     console.log(productData);
 
-    instance.post(`/work`, productData, {
-      headers: { "Content-Type": "application/json" }
-    })
-    .then((res) => {
+    instance
+      .post(`/work`, productData, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
         setPostProduct(res?.data);
-        navigate("/")
+        navigate("/");
         console.log(res?.data);
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
-    })
+      });
   };
   return (
     <div className="create_product">
       <form onSubmit={handlePostProduct}>
-        <h1>Create</h1>
+        <h1>Create Product</h1>
         <div>
-          <label htmlFor="">Upload Image</label>
-          <input type="file" />
+          <label htmlFor="file">
+            {photo ? (
+              <img src={postProduct?.image} alt="" />
+            ) : (
+              <MdOutlineFileUpload />
+            )}
+          </label>
+          <input
+            className="file"
+            type="file"
+            id="file"
+            name="file"
+            onChange={(e) => setPhoto(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">Enter title</label>
-          <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">Enter description</label>
-          <input type="text" onChange={(e) => setDescription(e.target.value)}/>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">Enter price</label>
-          <input type="number" onChange={(e) => setPrice(e.target.value)}/>
+          <input
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
